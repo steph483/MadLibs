bb0eaf4075ea7a4640894ea4c96e626375abf57f
steph483 <senriquez3191@gmail.com>
2022-02-12 12:31:25 -0800

    MadsLibs sample project

---
+++ Mad_Libs.py
@@ -0,0 +1,45 @@
+def main():
+    # Introduction
+    
+    print("Hello and welcome to your new MadLibs game.")
+    print("You can choose to do a happy or sad MadLib.")
+    userChoice = input("Which one do you want? ")
+    
+
+    #Getting user input    
+    if userChoice in ['sad', 'happy']:
+        print("Great selection!")
+        user_animal = input("Choose an animal: ")
+        user_verb = input("Choose a gerund: ")
+        user_location = input("Choose a location: ")      
+        
+    else:
+        print("Please try again")
+        print("-")
+        main()
+
+    #Sending user input to correct category
+    if userChoice == 'sad':
+        sad(user_animal, user_location, user_verb)
+    elif userChoice == 'happy':
+        happy(user_animal, user_location, user_verb)
+
+    play_again = input("Thanks for playing. Want to play again? (y/n) ")
+    if play_again == 'y':
+        main()
+    else:
+        print("bye")
+
+
+
+def happy(user_animal, user_location, user_verb):
+    print("There was one a {} that lived in {}.".format(user_animal, user_location))
+    print("The {} always showed their happiness by {} because their owner always took them out to walk.".format(user_animal, user_verb))
+    
+
+def sad(user_animal, user_location, user_verb):
+    print("There was one a {} that lived in {}.".format(user_animal, user_location))
+    print("Since the {}'s owner never paid attention to them, they were always {} and to try to get their attention.".format(user_animal, user_verb))
+
+if __name__ == "__main__":
+    main()
